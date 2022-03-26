0a57968727ec6cd922c4cc2d73c26ab3f078a075
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-26 12:07:50 -0300

    removido comentarios

--- index.js
+++ index.js
@@ -58,34 +58,6 @@ function checkRomanNumerals(numerals) {
       return 0;
   }
 }
-// seiLa(valor);
 
-// function seiLa(valor) {
-//   let verifica = 0;
-//   let testa = "";
 
-//   for (let i = 0; i < valor.length; i++) {
-//     if (valor[i] === valor[i + 1]) {
-//       verifica += converteAlgarismoIguais(valor[i]);
-//       testa = verifica.toString();
-//       // console.log(testa[0]);
-//     } else if (testa[0] === "2" && valor[i] === valor[i - 1]) {
-//       verifica += converteAlgarismoIguais(valor[i]);
-//     }
-//   }
-
-//   console.log(verifica);
-// }
-
-// function converteAlgarismoIguais(valor) {
-//   switch (valor) {
-//     case "I":
-//       return 1;
-//     case "X":
-//       return 10;
-//     case "C":
-//       return 100;
-//     case "M":
-//       return 1000;
-//   }
-// }
+console.log("X" + "C");
