08b686a5f1413a81aa8f469309b9ad0736c8d569
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-26 10:33:24 -0300

    logica converter algarismos iguais

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const value1 = "XX";
 const value2 = "I";
 
-valor = "XX"
+const valor = "CC";
 
-function (valor)
-valor[i] === valor[i + 1]
-converteAlgarismoIguais(valor[i])
+seiLa(valor);
 
+function seiLa(valor) {
+  let verifica = 0;
+  let testa = "";
 
+  for (let i = 0; i < valor.length; i++) {
+    if (valor[i] === valor[i + 1]) {
+      verifica += converteAlgarismoIguais(valor[i]);
+      testa = verifica.toString();
+      // console.log(testa[0]);
+    } else if (testa[0] === "2" && valor[i] === valor[i - 1]) {
+      verifica += converteAlgarismoIguais(valor[i]);
+    }
+  }
 
-converteAlgarismoIguais (valor1) {
+  console.log(verifica);
+}
+
+function converteAlgarismoIguais(valor) {
   switch (valor) {
-    case 'I':
-      1
-    case 'X':
-      10
-    case 'C':
-      100
-    case 'M':
-      1000
+    case "I":
+      return 1;
+    case "X":
+      return 10;
+    case "C":
+      return 100;
+    case "M":
+      return 1000;
   }
 }
