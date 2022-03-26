b87cd22ff84a7137521c7068ad4b9157c1cf3b9f
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-26 09:55:34 -0300

    [plan] converte algarismo iguais em sequencia

---
+++ index.js
@@ -0,0 +1,23 @@
+const value1 = "XX";
+const value2 = "I";
+
+valor = "XX"
+
+function (valor)
+valor[i] === valor[i + 1]
+converteAlgarismoIguais(valor[i])
+
+
+
+converteAlgarismoIguais (valor1) {
+  switch (valor) {
+    case 'I':
+      1
+    case 'X':
+      10
+    case 'C':
+      100
+    case 'M':
+      1000
+  }
+}
