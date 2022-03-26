4dbff76b1ca696e29b254177ab9777e97a6014b1
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-26 11:56:41 -0300

    fundamento conceito da nova logica

--- index.js
+++ index.js
@@ -1,36 +1,64 @@
-const value1 = "XX";
-const value2 = "I";
+/**
+ * Verifica quando os valores são
+ * "IV", 4
+ * "IX", 9
+ * "XL", 40
+ * "XC", 90
+ * "CD", 400
+ * "CM", 900
+ */
 
-const valor = "CC";
+let sym = [
+  "I",
+  "IV",
+  "V",
+  "IX",
+  "X",
+  "XL",
+  "L",
+  "XC",
+  "C",
+  "CD",
+  "D",
+  "CM",
+  "M",
+];
 
-seiLa(valor);
 
-function seiLa(valor) {
-  let verifica = 0;
-  let testa = "";
 
-  for (let i = 0; i < valor.length; i++) {
-    if (valor[i] === valor[i + 1]) {
-      verifica += converteAlgarismoIguais(valor[i]);
-      testa = verifica.toString();
-      // console.log(testa[0]);
-    } else if (testa[0] === "2" && valor[i] === valor[i - 1]) {
-      verifica += converteAlgarismoIguais(valor[i]);
-    }
-  }
-
-  console.log(verifica);
-}
-
-function converteAlgarismoIguais(valor) {
-  switch (valor) {
-    case "I":
-      return 1;
-    case "X":
-      return 10;
-    case "C":
-      return 100;
-    case "M":
-      return 1000;
   }
 }
+
+  }
+}
+// seiLa(valor);
+
+// function seiLa(valor) {
+//   let verifica = 0;
+//   let testa = "";
+
+//   for (let i = 0; i < valor.length; i++) {
+//     if (valor[i] === valor[i + 1]) {
+//       verifica += converteAlgarismoIguais(valor[i]);
+//       testa = verifica.toString();
+//       // console.log(testa[0]);
+//     } else if (testa[0] === "2" && valor[i] === valor[i - 1]) {
+//       verifica += converteAlgarismoIguais(valor[i]);
+//     }
+//   }
+
+//   console.log(verifica);
+// }
+
+// function converteAlgarismoIguais(valor) {
+//   switch (valor) {
+//     case "I":
+//       return 1;
+//     case "X":
+//       return 10;
+//     case "C":
+//       return 100;
+//     case "M":
+//       return 1000;
+//   }
+// }
