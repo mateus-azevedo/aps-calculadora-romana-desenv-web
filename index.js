822d5a2c31cb52c63b395630f419f22991bdb492
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-26 12:06:04 -0300

    funcao verifica mais de um algaritmo romano

--- index.js
+++ index.js
@@ -24,11 +24,38 @@ let sym = [
   "M",
 ];
 
+const num = "XXC";
 
+let i = 0;
+let valueInDecimal = 0;
+while (i < num.length) {
+  let current = i;
+  let next = i + 1;
+  let romanException = num[current] + num[next];
 
+  if (checkRomanNumerals(romanException)) {
+    valueInDecimal += checkRomanNumerals(romanException);
+    i += 2;
+    continue;
   }
 }
 
+function checkRomanNumerals(numerals) {
+  switch (numerals) {
+    case numerals === "IV":
+      return 4;
+    case numerals === "IX":
+      return 9;
+    case numerals === "XL":
+      return 40;
+    case numerals === "XC":
+      return 90;
+    case numerals === "CD":
+      return 400;
+    case numerals === "CM":
+      return 900;
+    default:
+      return 0;
   }
 }
 // seiLa(valor);
