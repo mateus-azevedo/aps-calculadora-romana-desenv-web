1684c68ce609753352625f3952667742174f5a7d
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-27 14:31:40 -0300

    funcao converte decimal para romano comentada

--- main.js
+++ main.js
@@ -132,3 +132,41 @@ function product(multiplier, multiplicand) {
 function quotient(numerator, denominator) {
   return numerator / denominator;
 }
+
+/**
+ * let number = 3549;
+
+function printRoman(number) {
+  let num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let sym = [
+    "I",
+    "IV",
+    "V",
+    "IX",
+    "X",
+    "XL",
+    "L",
+    "XC",
+    "C",
+    "CD",
+    "D",
+    "CM",
+    "M",
+  ];
+
+  for (let i = 12; number > 0; i--) {
+    let div = Math.floor(number / num[i]);
+    if (i == 10) {
+      console.log(div);
+      console.log(number);
+      console.log(number % num[i]);
+    }
+    number = number % num[i];
+    while (div--) {
+      // console.log(sym[i]);
+    }
+  }
+}
+
+printRoman(number);
+ */
